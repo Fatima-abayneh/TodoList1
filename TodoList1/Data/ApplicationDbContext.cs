ab368a19649740fe1ee92acb8233942a2bc56e71
Fatima Abayneh <fatumaabayneh093@gmail.com>
2024-11-12 10:47:55 +0300

    model updated

---
+++ TodoList1/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using TodoList1.Models;
+
+namespace TodoList1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
---
+++ TodoList1/Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿namespace TodoList1.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public int Status { get; set; }
+    }
+}
