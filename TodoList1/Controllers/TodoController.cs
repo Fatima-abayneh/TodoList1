78820de2883982b507e1dc73e61239575f32e738
Fatima Abayneh <fatumaabayneh093@gmail.com>
2025-01-03 14:14:33 +0300

    finalCommit

--- TodoList1/Controllers/TodoController.cs
+++ TodoList1/Controllers/TodoController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 using TodoList1.Data;
 using TodoList1.Models;
 
@@ -12,10 +14,22 @@ namespace TodoList1.Controllers
         {
             _context = context;
         }
-        public IActionResult Index()
+        public IActionResult Index(string searchString)
         {
-            var todoItems = _context.TodoItems.ToList();
-            return View(todoItems);
+            if (_context.TodoItems == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.TodoItems'  is null.");
+            }
+
+            var todoitem = from m in _context.TodoItems
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                todoitem = todoitem.Where(s => s.Title!.ToUpper().Contains(searchString.ToUpper()));
+            }
+
+            return View(todoitem.ToList());
         }
        
         [HttpPost]
@@ -27,6 +41,86 @@ namespace TodoList1.Controllers
                 _context.SaveChanges();
             }
             return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.TodoItems == null)
+            {
+                return NotFound();
+            }
+
+            var todoitem = await _context.TodoItems.FindAsync(id);
+            if (todoitem == null)
+            {
+                return NotFound();
+            }
+            return View(todoitem);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description, DueDate, IsCompleted")] TodoItem todoitem)
+        {
+            if (id != todoitem.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(todoitem);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TodoitemExists(todoitem.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(todoitem);
+        }
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.TodoItems == null)
+            {
+                return NotFound();
+            }
+
+            var todoitem = await _context.TodoItems
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (todoitem == null)
+            {
+                return NotFound();
+            }
+
+            return View(todoitem);
+        }
+
+        // POST: Categories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.TodoItems == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Categories'  is null.");
+            }
+            var category = await _context.TodoItems.FindAsync(id);
+            if (category != null)
+            {
+                _context.TodoItems.Remove(category);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
 
         [HttpPost]
@@ -52,6 +146,10 @@ namespace TodoList1.Controllers
             }
 
             return RedirectToAction("Index");
+        }
+        private bool TodoitemExists(int id)
+        {
+            return (_context.TodoItems?.Any(e => e.Id == id)).GetValueOrDefault();
         }
     }
 }
